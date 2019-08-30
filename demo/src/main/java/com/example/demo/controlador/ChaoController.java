64b24f93d683e1db61523e41beb5c2b2b90a0ef8
Jorge Antonio Figueroa Aqueveque <jorgef1987@gmail.com>
2019-08-30 19:13:32 -0400

    Se agrega controlador chao que se despide del cliente

---
+++ demo/src/main/java/com/example/demo/controlador/ChaoController.java
@@ -0,0 +1,11 @@
+package com.example.demo.controlador;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChaoController {
+
+    public static void main(String[] args) {
+        System.out.println("Chao");
+    }
+}
